6471608973eff9c6eb6e68ce33a8e32175cc7104
webuser <ryan@virtuecenter.com>
2013-11-24 17:17:18 -0500

    minor change

--- src/UploadWrapper/UploadWrapper.php
+++ src/UploadWrapper/UploadWrapper.php
@@ -2,8 +2,14 @@
 namespace UploadWrapper;
 
 class UploadWrapper {
+	private $root;
+
+	public function __construct ($root) {
+		$this->root = $root;
+	}
+
 	public function storage ($path) {
-		return new \Upload\Storage\FileSystem($path);
+		return new \Upload\Storage\FileSystem($this->root . $path);
 	}
 
 	public function file ($field, $storage) {
