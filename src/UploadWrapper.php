6d94b374a7c3bb74535bb5725c8585203b374525
Ryan Mahoney <ryan@virtuecenter.com>
2014-12-24 16:07:05 -0500

    progress

--- src/UploadWrapper.php
+++ src/UploadWrapper.php
@@ -10,10 +10,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,25 +23,31 @@
  * THE SOFTWARE.
  */
 namespace Opine;
+
 use Upload\Storage\FileSystem;
 use Upload\File;
 
-class UploadWrapper {
+class UploadWrapper
+{
     private $root;
 
-    public function __construct ($root) {
+    public function __construct($root)
+    {
         $this->root = $root;
     }
 
-    public function storage ($path) {
-        $path = $this->root . $path;
+    public function storage($path)
+    {
+        $path = $this->root.$path;
         if (!file_exists($path)) {
-            mkdir ($path, 0777, true);
+            mkdir($path, 0777, true);
         }
+
         return new FileSystem($path, true);
     }
 
-    public function file ($field, $storage) {
+    public function file($field, $storage)
+    {
         return new File($field, $storage);
     }
 }
--- tests/UploadWrapperTest.php
+++ tests/UploadWrapperTest.php
@@ -3,11 +3,14 @@ namespace Opine;
 
 use PHPUnit_Framework_TestCase;
 
-class UploadWrapperTest extends PHPUnit_Framework_TestCase {
+class UploadWrapperTest extends PHPUnit_Framework_TestCase
+{
+    public function setup()
+    {
+    }
 
-    public function setup () {}
-
-    public function testSample () {
+    public function testSample()
+    {
         $this->assertTrue(true);
     }
 }
